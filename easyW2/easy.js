1e563446d1c32721fc4ecbef9b7a8a0ddbad4cc9
vjaxel34 <92274553+vjaxel34@users.noreply.github.com>
2022-01-19 11:27:39 -0500

    Hello

--- easyW2/easy.js
+++ easyW2/easy.js
@@ -1,3 +1,28 @@
-//VERY EASY: Write a function named min that takes two arguments and returns their minimum.
-const min = [5, 9];
-console.log(Math.min(...min));
+//EASY: Create an array of students holding their last name, first name, and age with 3 students. To validate, please log a greeting with the first name, last name and age of the 2nd student. The output should look like "Hello, my name is John Doe and I'm 19 years old."
+
+// let students =[
+//     {
+//         firstName: "Axel",
+//         lastName: "Vargas",
+//         age: 21
+//     }, 
+//     {
+//         firstName: "Dylan",
+//         last: "Rafard",
+//         age: 19
+//     },
+//     {
+//         firstName: "Josh",
+//         lastName: "Philips",
+//         age: 18
+//     }
+// ]
+
+// console.log(students[2]);
+// console.log(`Hello ${students.firstName[1]}`)
+
+let student1 = ["Axel", "Vargas", 21];
+let student2 =["Dylan", "Rafard", 19];
+let student3 = ["Josh", "Philips", 24];
+console.log(`Hello ${student2}`)
+console.log(`Hello ${student2[0]} ${student2[1]}, you will turn ${student2[2]} years old today!`);
